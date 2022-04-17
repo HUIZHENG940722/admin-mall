0387fa4b5bb9ec1deb01cf9b2e708b6fa61e969b
zhenghui <15270855286@163.com>
2022-04-17 22:52:47 +0800

    初始化登录页面组件

--- src/pages/login/index.vue
+++ src/pages/login/index.vue
@@ -1,9 +1,24 @@
 <template>
-  <p>登录页面</p>
+  <el-form :model="form">
+    <el-form-item type="text" label="用户名:">
+      <el-input v-model="form.username" name="username"></el-input>
+    </el-form-item>
+    <el-form-item label="密码:">
+      <el-input type="password" v-model="form.password" name="password"></el-input>
+    </el-form-item>
+    <el-form-item>
+      <el-button type="primary" @click="">登录</el-button>
+      <el-button>注册用户</el-button>
+    </el-form-item>
+  </el-form>
 </template>
 
-<script lang="ts">
-
+<script lang="ts" setup>
+  import {reactive} from 'vue'
+  const form = reactive({
+    username: '',
+    password: '',
+  });
 </script>
 
 <style scoped>
