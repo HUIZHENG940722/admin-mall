ab7553434ebeec6ac528a6d58448bda7a14a37a2
zhenghui <15270855286@163.com>
2022-05-01 19:33:08 +0800

    优化登录页面

--- src/pages/login/index.vue
+++ src/pages/login/index.vue
@@ -1,26 +1,73 @@
 <template>
-  <el-form :model="form">
-    <el-form-item type="text" label="用户名:">
-      <el-input v-model="form.username" name="username"></el-input>
-    </el-form-item>
-    <el-form-item label="密码:">
-      <el-input type="password" v-model="form.password" name="password"></el-input>
-    </el-form-item>
-    <el-form-item>
-      <el-button type="primary" @click="">登录</el-button>
-      <el-button>注册用户</el-button>
-    </el-form-item>
-  </el-form>
+  <el-card class="login-form-layout">
+    <el-form :model="loginForm" :rules="loginRules" label-position="left">
+      <el-form-item prop="username">
+        <el-input
+            name="username"
+            type="text"
+            v-model="loginForm.username"
+            autocomplete="on"
+            placeholder="请输入用户名">
+        </el-input>
+      </el-form-item>
+      <el-form-item prop="password">
+        <el-input name="password"
+                  type="password"
+                  @keyup.enter="handleLogin"
+                  v-model="loginForm.password"
+                  autocomplete="on"
+                  placeholder="请输入密码">
+        </el-input>
+      </el-form-item>
+      <el-form-item style="margin-bottom: 60px;text-align: center">
+        <el-button style="width: 45%;" type="primary" :loading="loading" @click="handleLogin">登录</el-button>
+      </el-form-item>
+    </el-form>
+  </el-card>
 </template>
 
 <script lang="ts" setup>
-  import {reactive} from 'vue'
-  const form = reactive({
-    username: '',
-    password: '',
-  });
+import {reactive, ref} from 'vue';
+const validateUsername = () => {
+
+};
+const validatePassword = () => {
+
+}
+const loginRules = reactive({
+  username: [
+    {
+      required: true,
+      trigger: 'blur',
+      validator: validateUsername
+    },
+    {
+      required: true,
+      trigger: 'blur',
+      validator: validatePassword
+    },
+  ],
+  password: []
+});
+const loading = ref(false);
+const loginForm = reactive({
+  username: '',
+  password: ''
+});
+
+const handleLogin = ()=> {
+
+};
+
 </script>
 
 <style scoped>
-
+.login-form-layout {
+  position: absolute;
+  left: 0;
+  right: 0;
+  width: 360px;
+  margin: 140px auto;
+  border-top: 10px solid #409EFF;
+}
 </style>
