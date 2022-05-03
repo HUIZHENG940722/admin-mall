ad9885fd7ea04943727eeecccf2fd6cbea81c4fc
zhenghui <15270855286@163.com>
2022-05-03 20:33:58 +0800

    初始化response拦截器

--- src/utils/httpClientUtils.ts
+++ src/utils/httpClientUtils.ts
@@ -1,4 +1,6 @@
 import axios from 'axios';
+import { ElMessage} from 'element-plus';
+import {useStore} from "vuex";
 
 const httpClientUtils = axios.create({
     baseURL: 'http://localhost:8080',
@@ -6,6 +8,31 @@ const httpClientUtils = axios.create({
     headers: {
         'Content-type':'application/json;charset=utf-8',
     }
+});
+
+// response拦截器
+httpClientUtils.interceptors.response.use(response => {
+    const resp = response.data;
+    let store = useStore();
+    if (resp.code !== "200") {
+        ElMessage({
+            message: resp.message,
+            type: 'error',
+            duration: 3*1000
+        });
+        return Promise.reject('error')
+    } else {
+        return resp;
+    }
+
+}, error => {
+    console.log('err' + error)// for debug
+    ElMessage({
+        message: error.message,
+        type: 'error',
+        duration: 3 * 1000
+    })
+    return Promise.reject(error)
 });
 
 export default httpClientUtils;
